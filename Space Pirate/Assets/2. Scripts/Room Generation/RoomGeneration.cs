ff32342c7209d1dc2800f9086781fcd273327863
Keegan77 <keegansb77@gmail.com>
2023-01-21 18:53:25 -0500

    proto random generation (not working atm)

--- Space Pirate/Assets/2. Scripts/Room Generation/RoomGeneration.cs
+++ Space Pirate/Assets/2. Scripts/Room Generation/RoomGeneration.cs
@@ -4,12 +4,74 @@ using UnityEngine;
 
 public class RoomGeneration : MonoBehaviour
 {
-    private RoomLists roomLists;
+    //public int openingDir;
+
+    private RoomLists rooms;
+    public enum direction {Up, Down, Left, Right};
+    // 0 --> needs bottom door
+    // 1 needs top door
+    // 2 needs right door
+    // 3 needs left door
+
+    public direction openingDir;
+    private int rand;
+    public bool spawned;
 
     private void Awake()
     {
-        roomLists = GetComponent<RoomLists>();
+        rooms = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomLists>();
+    }
+    private void Start()
+    {
+        Invoke("GenerateRooms", .1f);
     }
 
+
+    private void GenerateRooms()
+    {
+        if (!spawned)
+        {
+            switch (openingDir)
+            {
+                case direction.Up:
+                    {
+                        SpawnRoom(rooms.bottomRooms);
+                        break;
+                    }
+                case direction.Left:
+                    {
+                        SpawnRoom(rooms.rightRooms);
+                        break;
+                    }
+                case direction.Right:
+                    {
+                        SpawnRoom(rooms.leftRooms);
+                        break;
+                    }
+                case direction.Down:
+                    {
+                        SpawnRoom(rooms.topRooms);
+                        break;
+                    }
+            }
+            
+        }
+
+    }
+    private void SpawnRoom(GameObject[] typeOfRoom)
+    {
+        rand = Random.Range(0, typeOfRoom.Length);
+        Instantiate(typeOfRoom[rand], transform.position, typeOfRoom[rand].transform.rotation);
+        spawned = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpawnNode"))
+        {
+            Debug.Log("Destroyed");
+            Destroy(gameObject);
+        }
+    }
 
 }
