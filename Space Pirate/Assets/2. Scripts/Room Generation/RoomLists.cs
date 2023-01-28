936fa214dd908cdd25ab2dd43e2b242cb1325881
Keegan77 <keegansb77@gmail.com>
2023-01-27 20:33:21 -0500

    Revert "Added in all possible rooms for now"
    This reverts commit 0665d947bd60ce31b00e76995f819423f614df1f.

--- Space Pirate/Assets/2. Scripts/Room Generation/RoomLists.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RoomLists : MonoBehaviour
-{
-    public GameObject[] bottomRooms, topRooms, leftRooms, rightRooms;
-}
