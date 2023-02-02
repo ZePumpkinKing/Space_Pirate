d948ca681f827451142ea2217dd1df7693c98809
ZePumpkinKing <79880943+ZePumpkinKing@users.noreply.github.com>
2023-02-01 21:35:30 -0500

    turrets!

--- Space Pirate/Assets/2. Scripts/Enemies/Turret.cs
+++ Space Pirate/Assets/2. Scripts/Enemies/Turret.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Turret : MonoBehaviour {
+    [SerializeField] Transform mount;
+    [SerializeField] Transform cannon;
+    [SerializeField] Transform sensor;
+
+    [SerializeField] GameObject projectile;
+
+    [SerializeField] bool laserSight;
+
+    Transform player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        cannon.transform.LookAt(player, transform.up);
+
+        mount.Rotate(transform.up, cannon.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y);
+
+        cannon.transform.LookAt(player, transform.up);
+
+        if (laserSight) {
+            Debug.DrawLine(sensor.position, player.position, Color.red);
+        }
     }
 }
