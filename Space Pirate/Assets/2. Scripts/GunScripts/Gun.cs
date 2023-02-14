c988942b74aface560c979b7c2e8d0431bb6cee6
LuckHeart <Cowtastik@icloud.com>
2023-02-14 01:02:07 -0500

    Revert "Merge pull request #14 from ZePumpkinKing/Keegan"
    This reverts commit e8e9d64dd7b1f2bae53bd27d5138b8cc238bd3ba, reversing
    changes made to 79f7ec88e06508acbcb30f8d7c9595af1b5e2567.

--- Space Pirate/Assets/2. Scripts/GunScripts/Gun.cs
+++ Space Pirate/Assets/2. Scripts/GunScripts/Gun.cs
@@ -2,30 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
-    [HideInInspector] public GunData currentGun;
-    [SerializeField] GunData[] guns;
-    [SerializeField] private GameObject[] gunObjs;
-    private GameObject currentGunObj;
+
+    [SerializeField] GunData currentGun;
+    //[SerializeField] GunData[] guns;
+    public enum guns
+    {
+        pistol,
+        blunderbus,
+        shotgun
+    }
+
     Input input;
     Transform castPoint;
     [SerializeField] private Transform gunTip;
 
+
     float timeSinceLastShot;
+    float currentAmmo;
     bool reloading;
     float autoShooting;
-    InputAction activeWeapon;
 
     private Recoil recoilScript;
     private void Awake()
     {
-        gunObjs = GameObject.FindGameObjectsWithTag("Gun");
-
-        currentGun = guns[0];
-        currentGunObj = gunObjs[0];
         input = new Input();
         recoilScript = FindObjectOfType<Recoil>();
         castPoint = GameObject.FindGameObjectWithTag("CastPoint").GetComponent<Transform>();
@@ -35,23 +37,15 @@ public class Gun : MonoBehaviour
         }
 
         input.Gameplay.Interact.performed += context => StartReload();
-        input.Gameplay.Weapon.performed += FindWeapon;
     }
     private void Start()
     {
-        for (int i = 0; i < gunObjs.Length; i++) //
-        {
-            guns[i].currentAmmo = guns[i].magCapacity;
-            if (i > 0)
-            {
-                SetInactive(gunObjs[i]);//set only one gun to active
-            }
-            
-        }
+        currentAmmo = currentGun.magCapacity;
     }
     private void Update()
     {
         timeSinceLastShot += Time.deltaTime;
+        Debug.DrawRay(castPoint.position, castPoint.forward);
         if (currentGun.automatic)
         {
             autoShooting = input.Gameplay.Fire.ReadValue<float>();
@@ -65,18 +59,20 @@ public class Gun : MonoBehaviour
     public void Shoot()
     {
         
-        if (currentGun.currentAmmo > 0)
+        if (currentAmmo > 0)
         {
             if (CanShoot())
             {
                 recoilScript.FireRecoil();
+                Debug.Log(currentAmmo);
                 if (Physics.Raycast(castPoint.position, castPoint.forward, out RaycastHit hit, currentGun.maxDistance))
                 {
                     IDamageable damageable = hit.transform.GetComponent<IDamageable>();
                     damageable?.TakeDamage(currentGun.damage);
                 }
-                currentGun.currentAmmo--;
+                currentAmmo--;
                 timeSinceLastShot = 0;
+                //OnGunShot();
             }
         }
 
@@ -92,68 +88,19 @@ public class Gun : MonoBehaviour
     {
         reloading = true;
         yield return new WaitForSeconds(currentGun.reloadTime);
-        currentGun.currentAmmo = currentGun.magCapacity;
+        currentAmmo = currentGun.magCapacity;
         
         reloading = false;
     }
-    private void FindWeapon(InputAction.CallbackContext context)
-    {
-        switch(activeWeapon.ReadValue<Vector2>().x)
-        {
-            case 1:
-                SwitchWeapon(0);
-                break;
-            case -1:
-                SwitchWeapon(1);
-                break;
 
-        }
-        switch(activeWeapon.ReadValue<Vector2>().y)
-        {
-            case 1:
-                SwitchWeapon(2);
-                break;
-            case -1:
-                SwitchWeapon(3);
-                break;
-        }
-        
-        if (!currentGun.automatic)
-        {
-            input.Gameplay.Fire.performed += context => Shoot();
-        }
-    }
-    private void SwitchWeapon(int gunId)
-    {
-        if (!reloading && timeSinceLastShot > 1f / (currentGun.fireRateRPM / 60f)) //only switch guns if we arent currently reloading, or on shot cooldown, prevents weird edge cases
-        {
-            SwitchGunModel(currentGunObj, gunObjs[gunId]);
-            currentGunObj = gunObjs[gunId];
-            currentGun = guns[gunId];
-        }
-
-    }
-    private void SwitchGunModel(GameObject prevModel,GameObject newModel)
-    {
-        prevModel.SetActive(false);
-        newModel.SetActive(true);
-    }
-
-    private void SetInactive(GameObject obj)
-    {
-        obj.SetActive(false);
-    }
 
     private void OnEnable()
     {
         input.Enable();
-        activeWeapon = input.Gameplay.Weapon;
-        activeWeapon.Enable();
     }
 
     private void OnDisable()
     {
         input.Disable();
-        activeWeapon.Disable();
     }
 }
