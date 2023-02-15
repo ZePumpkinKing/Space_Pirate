0bf18285196258d8dcbd9a267cdbfcb0092c4f53
Keegan77 <keegansb77@gmail.com>
2023-02-15 16:06:24 -0500

    better weapon sway and faster fire time

--- Space Pirate/Assets/2. Scripts/GunScripts/WeaponSway.cs
+++ Space Pirate/Assets/2. Scripts/GunScripts/WeaponSway.cs
@@ -7,15 +7,26 @@ public class WeaponSway : MonoBehaviour
 {
     Input input;
 
-    [Header("Sway Values")]
+    [Header("Position")]
     public float amount;
     public float maxAmount;
     public float smoothAmount;
+    [Header("Rotation")]
+    public float rotationAmount = 4f;
+    public float maxRotationAmount = 5f;
+    public float smoothRotation = 12f;
+
+    [Space]
+    public bool rotationX = true;
+    public bool rotationY = true;
+    public bool rotationZ = true;
+
 
     private Vector3 initPosition;
+    private Quaternion initRotation;
 
-    private float movementX;
-    private float movementY;
+    private float inputX;
+    private float inputY;
     private Vector2 movement;
 
     private void Awake()
@@ -25,20 +36,42 @@ public class WeaponSway : MonoBehaviour
     private void Start()
     {
         initPosition = transform.localPosition;
+        initRotation = transform.localRotation;
     }
     private void Update()
     {
         movement = input.Gameplay.Look.ReadValue<Vector2>();
-        movementX = movement.x;
-        movementY = movement.y;
+        CalculateSway();
+        MoveSway();
+        TiltSway();
 
-        float moveX = Mathf.Clamp(movementX * amount, -maxAmount, maxAmount);
-        float moveY = Mathf.Clamp(movementY * amount, -maxAmount, maxAmount);
+    }
+
+    private void CalculateSway()
+    {
+        inputX = movement.x;
+        inputY = movement.y;
+    }
+    private void MoveSway()
+    {
+        float moveX = Mathf.Clamp(inputX * amount, -maxAmount, maxAmount);
+        float moveY = Mathf.Clamp(inputY * amount, -maxAmount, maxAmount);
 
         Vector3 finalPos = new Vector3(moveX, moveY, 0);
 
         transform.localPosition = Vector3.Lerp(transform.localPosition, finalPos, Time.deltaTime * smoothAmount);
     }
+    private void TiltSway()
+    {
+        float tiltY = Mathf.Clamp(inputX * rotationAmount, -maxRotationAmount, maxRotationAmount);
+        float tiltX = Mathf.Clamp(inputY * rotationAmount, -maxRotationAmount, maxRotationAmount);
+
+        Quaternion finalRot = Quaternion.Euler(rotationX ? -tiltX : 0f, rotationY ? tiltY : 0f, rotationZ ? tiltY : 0f);
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, finalRot * initRotation, Time.deltaTime * smoothRotation);
+    }
+
+
     private void OnEnable()
     {
         input.Enable();
