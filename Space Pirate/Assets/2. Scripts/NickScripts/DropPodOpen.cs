ab5a1e4614852603cb69389615d3d2139c11c7c5
Beto556 <NicholasGreen556@gmail.com>
2023-04-27 12:12:01 -0400

    Reflection probe gaming

--- Space Pirate/Assets/2. Scripts/NickScripts/DropPodOpen.cs
+++ Space Pirate/Assets/2. Scripts/NickScripts/DropPodOpen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DropPodOpen : MonoBehaviour
 {
@@ -9,12 +10,35 @@ public class DropPodOpen : MonoBehaviour
 
     public Collider playerCollider;
 
+    public Camera swapCam; 
+
+    public Camera playerDefaultCam;
+
+    private bool entered;
+
+    Player player;
+
+    Input input;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<Player>();
+        input.Gameplay.Interact.performed += context => CameraSwap();
+        input = new Input();
+        swapCam.enabled = false;
+        playerDefaultCam.enabled = true;
         dropPodAnimator.SetBool("IsInRange", false);
     }
+    private void OnEnable() 
+    {
+        input.Enable();
+    }
 
+    private void OnDisable() 
+    {
+        input.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -25,11 +49,28 @@ public class DropPodOpen : MonoBehaviour
     {
         dropPodAnimator.SetBool("IsInRange", true);
 
+        entered = true;
+
        // Debug.Log("is in trigger :D");
     }
-
     private void OnTriggerExit(Collider playerCollider)
     {
+        entered = false;
         dropPodAnimator.SetBool("IsInRange", false);
     }
+
+
+    private void CameraSwap()
+    {
+        if (entered)
+        {
+            playerDefaultCam.enabled = false;
+            Destroy(player.gameObject);
+            Debug.Log("Camera Swap");
+            swapCam.enabled = true;
+        }
+        
+    }
+
+    
 }
